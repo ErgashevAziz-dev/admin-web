808178cc40cb8ebbfbb773a724b11ba436fae441
Aziz | Python Developer <ergashevazizdev@gmail.com>
2026-02-01 04:41:15 +0500

    Update models.py

--- menu/models.py
+++ menu/models.py
@@ -12,7 +12,7 @@ class Category(models.Model):
 class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
     name = models.CharField(max_length=100)
-    price = models.IntegerField(max_length=50)
+    price = models.IntegerField()
 
     def __str__(self):
         return self.name
